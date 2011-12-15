4e7e34d6a915fe90f7a2333830cd4a6f69fd4e08
Kyle Partridge <partkyle@gmail.com>
2011-12-14 21:24:50 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Griddit::Application.config.secret_token = 'e06b80b0875af979cb3d8cd5879137f8ccd2136d32cdf346b57e43bacff7a37f2eea44fdb36f1d2ec226fac73b658af1582a3eb5f6522039a28e43ddc7cb5a43'
